f0461330dafa38a7454e6051b6ec1d4cb3d25602
Vladimir Zaytsev <vladimir.zaytsev.m@gmail.com>
2024-01-17 08:08:23 -0600

    initial

---
+++ src/main.rs
@@ -0,0 +1,207 @@
+use std::io::Write;
+
+use clap::{Parser, Subcommand};
+use tokio_retry::strategy::ExponentialBackoff;
+use tokio_retry::Retry;
+use trunk_analytics_cli::bundler::BundlerUtil;
+use trunk_analytics_cli::scanner::{BundleRepo, EnvScanner, FileSet, FileSetCounter};
+use trunk_analytics_cli::types::BundleMeta;
+use trunk_analytics_cli::utils::{from_non_empty_or_default, parse_custom_tags};
+
+#[derive(Debug, Parser)]
+#[command(
+    version = "1.0",
+    name = "trunk-metrics-cli",
+    about = "Trunk Metrics CLI"
+)]
+struct Cli {
+    #[command(subcommand)]
+    pub command: Commands,
+}
+
+#[derive(Debug, Subcommand)]
+enum Commands {
+    #[clap(name = "upload")]
+    Upload {
+        #[arg(
+            long,
+            required = true,
+            value_delimiter = ',',
+            help = "Comma-separated list of glob paths to junit files."
+        )]
+        junit_paths: Vec<String>,
+        #[arg(long, help = "Organization url slug.")]
+        org_url_slug: String,
+        #[arg(
+            long,
+            required = true,
+            env = "TRUNK_API_TOKEN",
+            help = "Organization token. Defaults to TRUNK_API_TOKEN env var."
+        )]
+        token: String,
+        #[arg(long, help = "Path to repository root. Defaults to current directory.")]
+        repo_root: Option<String>,
+        #[arg(long, help = "Value to override URL of repository.")]
+        repo_url: Option<String>,
+        #[arg(long, help = "Value to override SHA of repository head.")]
+        repo_head_sha: Option<String>,
+        #[arg(long, help = "Value to override branch of repository head.")]
+        repo_head_branch: Option<String>,
+        #[arg(long, help = "Value to override commit epoch of repository head.")]
+        repo_head_commit_epoch: Option<String>,
+        #[arg(
+            long,
+            value_delimiter = ',',
+            help = "Comma separated list of custom tag=value pairs."
+        )]
+        tags: Vec<String>,
+        #[arg(long, help = "Print files which will be uploaded to stdout.")]
+        print_files: bool,
+        #[arg(long, help = "Run metrics CLI without uploading to API.")]
+        dry_run: bool,
+    },
+}
+
+const DEFAULT_API_ADDRESS: &str = "https://api.trunk.io:5022";
+// Tokio-retry uses base ^ retry * factor formula.
+// This will give us 8ms, 64ms, 512ms, 4096ms, 32768ms
+const RETRY_BASE_MS: u64 = 8;
+const RETRY_FACTOR: u64 = 1;
+const RETRY_COUNT: usize = 5;
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    setup_logger()?;
+    let cli = Cli::parse();
+    if let Err(e) = run(cli).await {
+        log::error!("Error: {:?}", e);
+        std::process::exit(exitcode::SOFTWARE);
+    }
+    Ok(())
+}
+
+async fn run(cli: Cli) -> anyhow::Result<()> {
+    let Commands::Upload {
+        junit_paths,
+        org_url_slug,
+        token,
+        repo_root,
+        repo_url,
+        repo_head_sha,
+        repo_head_branch,
+        repo_head_commit_epoch,
+        tags,
+        print_files,
+        dry_run,
+    } = cli.command;
+
+    let api_address = from_non_empty_or_default(
+        std::env::var("TRUNK_API_ADDRESS").ok(),
+        DEFAULT_API_ADDRESS.to_string(),
+        |s| s,
+    );
+
+    let tags = parse_custom_tags(&tags)?;
+
+    let repo = BundleRepo::try_read_from_root(
+        repo_root,
+        repo_url,
+        repo_head_sha,
+        repo_head_branch,
+        repo_head_commit_epoch,
+    )?;
+
+    if junit_paths.len() == 0 {
+        log::error!("No junit paths provided.");
+        return Ok(());
+    }
+    let mut file_counter = FileSetCounter::default();
+    let file_sets = junit_paths
+        .iter()
+        .map(|path| FileSet::scan_from_glob(&repo.repo_root, path.to_string(), &mut file_counter))
+        .collect::<anyhow::Result<Vec<FileSet>>>()?;
+
+    let envs = EnvScanner::scan_env();
+    let meta = BundleMeta {
+        org: org_url_slug.clone(),
+        repo: repo.clone(),
+        tags,
+        file_sets,
+        envs,
+    };
+    log::info!("Total files pack and upload: {}", file_counter.count_file());
+    if file_counter.count_file() == 0 {
+        log::warn!(
+            "No JUnit files found to pack and upload using globs: {:?}",
+            junit_paths
+        );
+    }
+
+    if print_files {
+        println!("Files to upload:");
+        for file_set in &meta.file_sets {
+            println!(
+                "  File set ({:?}): {}",
+                file_set.file_set_type, file_set.glob
+            );
+            for file in &file_set.files {
+                println!("    {}", file.original_path);
+            }
+        }
+    }
+
+    let bundle_temp_dir = tempfile::tempdir()?;
+    let bundle_time_file = bundle_temp_dir.path().join("bundle.tar.zstd");
+    let bundler = BundlerUtil::new(meta);
+    bundler.make_tarball(&bundle_time_file)?;
+    log::info!("Flushed temporary tarball to {:?}", bundle_time_file);
+
+    let upload = Retry::spawn(default_delay(), || {
+        trunk_metrics_uploader_lib::clients::get_bundle_upload_location(
+            &api_address,
+            &token,
+            &org_url_slug,
+            &repo.repo,
+        )
+    })
+    .await?;
+
+    if dry_run {
+        log::info!("Dry run, skipping upload.");
+        return Ok(());
+    }
+
+    Retry::spawn(default_delay(), || {
+        trunk_metrics_uploader_lib::clients::put_bundle_to_s3(&upload.url, &bundle_time_file)
+    })
+    .await?;
+
+    log::info!("Done");
+    Ok(())
+}
+
+fn default_delay() -> std::iter::Take<ExponentialBackoff> {
+    ExponentialBackoff::from_millis(RETRY_BASE_MS)
+        .factor(RETRY_FACTOR)
+        .take(RETRY_COUNT)
+}
+
+fn setup_logger() -> anyhow::Result<()> {
+    let mut builder = env_logger::Builder::new();
+    builder
+        .format(|buf, record| {
+            writeln!(
+                buf,
+                "{} [{}] - {}",
+                chrono::Local::now().format("%Y-%m-%dT%H:%M:%S"),
+                record.level(),
+                record.args()
+            )
+        })
+        .filter(None, log::LevelFilter::Info);
+    if let Ok(log) = std::env::var("TRUNK_LOG") {
+        builder.parse_filters(&log);
+    }
+    builder.init();
+    Ok(())
+}
---
+++ src/scanner.rs
@@ -0,0 +1,205 @@
+use regex::Regex;
+use serde::Serialize;
+
+use crate::constants::{ALLOW_LIST, ENVS_TO_GET};
+use crate::types::{BundledFile, FileSetType, Repo};
+use crate::utils::from_non_empty_or_default;
+
+pub const GIT_REMOTE_ORIGIN_URL_CONFIG: &str = "remote.origin.url";
+
+#[derive(Default, Debug)]
+pub struct FileSetCounter {
+    inner: usize,
+}
+
+impl FileSetCounter {
+    pub fn count_file(&mut self) -> usize {
+        let prev = self.inner;
+        self.inner += 1;
+        prev
+    }
+
+    pub fn get_count(&self) -> usize {
+        self.inner
+    }
+}
+
+#[derive(Debug, Serialize, Clone)]
+pub struct FileSet {
+    pub file_set_type: FileSetType,
+    pub files: Vec<BundledFile>,
+    pub glob: String,
+}
+
+impl FileSet {
+    /// Scan a file set from a glob path.
+    /// And generates file set using file counter.
+    ///
+    pub fn scan_from_glob(
+        repo_root: &str,
+        glob_path: String,
+        file_counter: &mut FileSetCounter,
+    ) -> anyhow::Result<FileSet> {
+        let path_to_scan = if !std::path::Path::new(&glob_path).is_absolute() {
+            std::path::Path::new(repo_root)
+                .join(&glob_path)
+                .to_str()
+                .expect("failed to convert path to string")
+                .to_string()
+        } else {
+            glob_path.clone()
+        };
+
+        let mut files = Vec::new();
+
+        glob::glob(&path_to_scan)?.try_for_each(|entry| {
+            let path = match entry {
+                Ok(path) => path,
+                Err(e) => {
+                    log::error!("Error scanning file set: {:?}", e);
+                    return Ok::<(), anyhow::Error>(());
+                }
+            };
+
+            if !path.is_file() {
+                return Ok::<(), anyhow::Error>(());
+            }
+
+            let original_path = path
+                .to_str()
+                .expect("failed to convert path to string")
+                .to_string();
+
+            // Check if file is allowed.
+            let mut is_allowed = false;
+            for allow in ALLOW_LIST {
+                let re = Regex::new(allow).unwrap();
+                if re.is_match(&original_path) {
+                    is_allowed = true;
+                    break;
+                }
+            }
+            if !is_allowed {
+                log::warn!("File {:?} from glob {:?} is not allowed", path, glob_path);
+                return Ok::<(), anyhow::Error>(());
+            }
+
+            // Save file under junit/0, junit/1, etc.
+            // This is to avoid having to deal with potential file name collisions.
+            files.push(BundledFile {
+                original_path,
+                path: format!("junit/{}", file_counter.count_file()),
+            });
+
+            Ok(())
+        })?;
+
+        Ok(FileSet {
+            file_set_type: FileSetType::Junit,
+            files,
+            glob: glob_path,
+        })
+    }
+}
+
+#[derive(Debug, Serialize, Clone)]
+pub struct BundleRepo {
+    pub repo: Repo,
+    pub repo_root: String,
+    pub repo_url: String,
+    pub repo_head_sha: String,
+    pub repo_head_branch: String,
+    pub repo_head_commit_epoch: i64,
+}
+
+impl BundleRepo {
+    /// Read important fields from git repo root.
+    ///
+    pub fn try_read_from_root(
+        in_repo_root: Option<String>,
+        in_repo_url: Option<String>,
+        in_repo_head_sha: Option<String>,
+        in_repo_head_branch: Option<String>,
+        in_repo_head_commit_epoch: Option<String>,
+    ) -> anyhow::Result<BundleRepo> {
+        let mut out_repo_url = in_repo_url.clone();
+        let mut out_repo_head_sha = in_repo_head_sha.clone();
+        let mut out_repo_head_branch = in_repo_head_branch.clone();
+        let mut out_repo_head_commit_epoch =
+            from_non_empty_or_default(in_repo_head_commit_epoch, None, |s| {
+                Some(
+                    s.parse::<i64>()
+                        .expect("failed to parse commit epoch override"),
+                )
+            });
+        let out_repo_root =
+            from_non_empty_or_default(in_repo_root, Self::default_to_working_directory(), |s| {
+                Some(s)
+            });
+
+        // If repo root found, try to get repo details from git.
+        if let Some(repo_root) = &out_repo_root {
+            // Read git repo.
+            log::info!("Reading git repo at {:?}", &repo_root);
+
+            let git_repo = gix::open(&repo_root)?;
+            let git_url = git_repo
+                .config_snapshot()
+                .string_by_key(GIT_REMOTE_ORIGIN_URL_CONFIG)
+                .map(|s| s.to_string());
+            let mut git_head = git_repo.head()?;
+            let git_head_sha = git_head.id().map(|id| id.to_string());
+            let git_head_branch = git_head.referent_name().map(|s| s.as_bstr().to_string());
+            let git_head_commit_time = git_head.peel_to_commit_in_place()?.time()?;
+
+            log::info!("Found git_url: {:?}", git_url);
+            log::info!("Found git_sha: {:?}", git_head_sha);
+            log::info!("Found git_branch: {:?}", git_head_branch);
+            log::info!("Found git_commit_time: {:?}", git_head_commit_time);
+
+            out_repo_url = from_non_empty_or_default(in_repo_url, git_url, |s| Some(s));
+            out_repo_head_sha =
+                from_non_empty_or_default(in_repo_head_sha, git_head_sha, |s| Some(s));
+            out_repo_head_branch =
+                from_non_empty_or_default(in_repo_head_branch, git_head_branch, |s| Some(s));
+            if out_repo_head_commit_epoch.is_none() {
+                out_repo_head_commit_epoch = Some(git_head_commit_time.seconds);
+            }
+        }
+
+        // Require URL which should be known at this point.
+        let repo_url = out_repo_url.expect("failed to get repo url");
+        let repo = Repo::from_url(&repo_url)?;
+
+        Ok(BundleRepo {
+            repo,
+            repo_root: out_repo_root.unwrap_or("".to_string()),
+            repo_url,
+            repo_head_branch: out_repo_head_branch.unwrap_or_default(),
+            repo_head_sha: out_repo_head_sha.expect("failed to get repo head sha"),
+            repo_head_commit_epoch: out_repo_head_commit_epoch
+                .expect("failed to get repo head commit time"),
+        })
+    }
+
+    fn default_to_working_directory() -> Option<String> {
+        std::env::current_dir()
+            .expect("failed to resolve working directory")
+            .to_str()
+            .map(|s| s.to_string())
+    }
+}
+
+pub struct EnvScanner;
+
+impl EnvScanner {
+    pub fn scan_env() -> std::collections::HashMap<String, String> {
+        let mut envs = std::collections::HashMap::with_capacity(ENVS_TO_GET.len());
+        for env in ENVS_TO_GET {
+            if let Ok(val) = std::env::var(env) {
+                envs.insert(env.to_string(), val);
+            }
+        }
+        envs
+    }
+}
