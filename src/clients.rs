3e61a68454f954528453e5f7a8c04670e2e00a15
riya-n <riya@trunk.io>
2024-01-26 11:37:07 -0800

    handle errors better

--- src/clients.rs
+++ src/clients.rs
@@ -7,6 +7,15 @@ use crate::types::{BundleUploadLocation, Repo};
 pub const TRUNK_API_TIMEOUT: std::time::Duration = std::time::Duration::from_secs(30);
 pub const TRUNK_API_TOKEN_HEADER: &str = "x-api-token";
 
+fn handle_upload_bundle_error_logs(status: Option<reqwest::StatusCode>) {
+    log::error!("Failed to create bundle upload. Status: {:?}", status);
+    if status == Some(reqwest::StatusCode::UNAUTHORIZED) {
+        log::error!("Your Trunk token may be incorrect - find it on the Trunk app (Settings -> Manage Organization -> Organization API Token -> View).");
+    } else if status == Some(reqwest::StatusCode::NOT_FOUND) {
+        log::error!("Your Trunk organization URL slug may be incorrect - find it on the Trunk app (Settings -> Manage Organization -> Organization Slug).");
+    }
+}
+
 pub async fn get_bundle_upload_location(
     api_address: &str,
     api_token: &str,
@@ -34,15 +43,15 @@ pub async fn get_bundle_upload_location(
     {
         Ok(resp) => resp,
         Err(e) => {
-            log::error!("Failed to create bundle upload. Status: {:?}", e.status());
-            if e.status() == Some(reqwest::StatusCode::UNAUTHORIZED) {
-                log::error!("Your Trunk token may be incorrect - find it on the Trunk app (Settings -> Manage Organization -> Organization API Token -> View).");
-            } else if e.status() == Some(reqwest::StatusCode::NOT_FOUND) {
-                log::error!("Your Trunk organization URL slug may be incorrect - find it on the Trunk app (Settings -> Manage Organization -> Organization Slug).");
-            }
+            handle_upload_bundle_error_logs(e.status());
             return Err(anyhow::anyhow!(e).context("Failed to create bundle upload"));
         }
     };
+
+    if resp.status() != reqwest::StatusCode::OK {
+        handle_upload_bundle_error_logs(Some(resp.status()));
+        return Err(anyhow::anyhow!(resp.status()).context("Failed to create bundle upload"));
+    }
 
     resp.json::<BundleUploadLocation>()
         .await
