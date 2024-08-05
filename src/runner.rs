faf6892cbf7d178162ecaab22ff51bd2c2371d66
Federico Weber <federico@trunk.io>
2024-08-05 16:35:47 -0700

    break down run_test_command logic

--- src/runner.rs
+++ src/runner.rs
@@ -20,6 +20,21 @@ pub async fn run_test_command(
     codeowners_path: Option<String>,
 ) -> anyhow::Result<RunResult> {
     let start = SystemTime::now();
+    let exit_code = run_test_and_get_exit_code(command, args).await?;
+    log::info!("Command exit code: {}", exit_code);
+    let file_sets = get_file_sets(repo, output_paths, team.clone(), codeowners_path.clone())?;
+    let failures = if exit_code != EXIT_SUCCESS {
+        get_failures(file_sets, start).await?
+    } else {
+        Vec::new()
+    };
+    Ok(RunResult {
+        exit_code,
+        failures,
+    })
+}
+
+async fn run_test_and_get_exit_code(command: &String, args: Vec<&String>) -> anyhow::Result<i32> {
     let mut child = Command::new(command)
         .args(args)
         .stdout(Stdio::inherit())
@@ -30,101 +45,98 @@ pub async fn run_test_command(
         Ok(result) => result,
         Err(e) => {
             log::error!("Error waiting for execution: {}", e);
-            return Ok(RunResult {
-                exit_code: EXIT_FAILURE,
-                failures: Vec::new(),
-            });
+            return Ok(EXIT_FAILURE);
         }
     };
+    Ok(result.code().unwrap_or(EXIT_FAILURE))
+}
+
+fn get_file_sets(
+    repo: &BundleRepo,
+    output_paths: Vec<&String>,
+    team: Option<String>,
+    codeowners_path: Option<String>,
+) -> anyhow::Result<Vec<FileSet>> {
+    let mut file_counter = FileSetCounter::default();
+    let file_sets = output_paths
+        .iter()
+        .map(|path| {
+            FileSet::scan_from_glob(
+                &repo.repo_root,
+                path.to_string(),
+                &mut file_counter,
+                team.clone(),
+                codeowners_path.clone(),
+            )
+        })
+        .collect::<anyhow::Result<Vec<FileSet>>>()?;
+    Ok(file_sets)
+}
+
+async fn get_failures(
+    file_sets: Vec<FileSet>,
+    start: SystemTime,
+) -> anyhow::Result<Vec<Test>> {
     let mut failures = Vec::<Test>::new();
-    if !result.success() {
-        let mut file_counter = FileSetCounter::default();
-        let file_sets = match output_paths
-            .iter()
-            .map(|path| {
-                FileSet::scan_from_glob(
-                    &repo.repo_root,
-                    path.to_string(),
-                    &mut file_counter,
-                    team.clone(),
-                    codeowners_path.clone(),
-                )
-            })
-            .collect::<anyhow::Result<Vec<FileSet>>>()
-        {
-            Ok(file_sets) => file_sets,
-            Err(e) => {
-                log::error!("Error scanning file sets: {}", e);
-                return Ok(RunResult {
-                    exit_code: result.code().unwrap_or(EXIT_FAILURE),
-                    failures,
-                });
-            }
-        };
-        for file_set in &file_sets {
-            for file in &file_set.files {
-                log::info!("Checking file: {}", file.original_path);
-                let metadata = match metadata(&file.original_path) {
-                    Ok(metadata) => metadata,
-                    Err(e) => {
-                        log::warn!("Error getting metadata: {}", e);
-                        continue;
-                    }
-                };
-                let time = match metadata.modified() {
-                    Ok(time) => time,
-                    Err(e) => {
-                        log::warn!("Error getting modified time: {}", e);
-                        continue;
-                    }
-                };
-                // skip files that were last modified before the test started
-                if time <= start {
-                    log::info!(
-                        "Skipping file because of lack of modification: {}",
-                        file.original_path
-                    );
+    for file_set in &file_sets {
+        for file in &file_set.files {
+            log::info!("Checking file: {}", file.original_path);
+            let metadata = match metadata(&file.original_path) {
+                Ok(metadata) => metadata,
+                Err(e) => {
+                    log::warn!("Error getting metadata: {}", e);
                     continue;
                 }
-                let file = match std::fs::File::open(&file.original_path) {
-                    Ok(file) => file,
-                    Err(e) => {
-                        log::warn!("Error opening file: {}", e);
-                        continue;
-                    }
-                };
-                let reader = std::io::BufReader::new(file);
-                let junitxml = match junit_parser::from_reader(reader) {
-                    Ok(junitxml) => junitxml,
-                    Err(e) => {
-                        log::warn!("Error parsing junitxml: {}", e);
-                        continue;
-                    }
-                };
-                for suite in junitxml.suites {
-                    let parent_name = suite.name;
-                    for case in suite.cases {
-                        let failure = case.status.is_failure();
-                        if failure {
-                            let name = case.original_name;
-                            log::debug!("Test failed: {} -> {}", parent_name, name);
-                            failures.push(Test {
-                                parent_name: parent_name.clone(),
-                                name: name.clone(),
-                                class_name: case.classname.clone(),
-                                file: case.file.clone(),
-                            });
-                        }
+            };
+            let time = match metadata.modified() {
+                Ok(time) => time,
+                Err(e) => {
+                    log::warn!("Error getting modified time: {}", e);
+                    continue;
+                }
+            };
+            // skip files that were last modified before the test started
+            if time <= start {
+                log::info!(
+                    "Skipping file because of lack of modification: {}",
+                    file.original_path
+                );
+                continue;
+            }
+            let file = match std::fs::File::open(&file.original_path) {
+                Ok(file) => file,
+                Err(e) => {
+                    log::warn!("Error opening file: {}", e);
+                    continue;
+                }
+            };
+            let reader = std::io::BufReader::new(file);
+            let junitxml = match junit_parser::from_reader(reader) {
+                Ok(junitxml) => junitxml,
+                Err(e) => {
+                    log::warn!("Error parsing junitxml: {}", e);
+                    continue;
+                }
+            };
+            for suite in junitxml.suites {
+                let parent_name = suite.name;
+                for case in suite.cases {
+                    let failure = case.status.is_failure();
+                    if failure {
+                        let name = case.original_name;
+                        log::debug!("Test failed: {} -> {}", parent_name, name);
+                        failures.push(Test {
+                            parent_name: parent_name.clone(),
+                            name: name.clone(),
+                            class_name: case.classname.clone(),
+                            file: case.file.clone(),
+                        });
                     }
                 }
             }
         }
     }
-    let exit_code = result.code().unwrap_or(EXIT_FAILURE);
-    Ok(RunResult {
-        exit_code,
-        failures,
-    })
+    Ok(failures)
 }
 
 pub async fn run_quarantine(
@@ -136,6 +148,7 @@ pub async fn run_quarantine(
     delay: std::iter::Take<ExponentialBackoff>,
 ) -> anyhow::Result<QuarantineRunResult> {
     // check with the API if the group is quarantined
+    log::info!("Checking quarantine status for failures: {:?}", run_result.failures);
     let quarantine_results = if run_result.failures.is_empty() {
         QuarantineBulkTestStatus {
             group_is_quarantined: false,
