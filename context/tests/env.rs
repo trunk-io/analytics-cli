091e410d9f1e409ce6ebdc112a74b32b288e31f4
Tyler Jang <tyler@trunk.io>
2024-11-19 14:42:31 -0800

    fix

--- context/tests/env.rs
+++ context/tests/env.rs
@@ -160,6 +160,7 @@ fn test_simple_github() {
     let env_vars = EnvVars::from_iter(
         vec![
             (String::from("GITHUB_ACTIONS"), String::from("true")),
+            (String::from("GITHUB_EVENT_NAME"), String::from("schedule")),
             (String::from("GITHUB_RUN_ID"), String::from(&run_id)),
             (String::from("GITHUB_ACTOR"), String::from(&actor)),
             (String::from("GITHUB_REPOSITORY"), String::from(&repository)),
@@ -188,6 +189,91 @@ fn test_simple_github() {
             branch: Some(branch),
             branch_class: None,
             pr_number: None,
+            actor: Some(actor),
+            committer_name: None,
+            committer_email: None,
+            author_name: None,
+            author_email: None,
+            commit_message: None,
+            title: None,
+            workflow: Some(workflow),
+            job: Some(job),
+        }
+    );
+
+    let env_validation = env::validator::validate(&ci_info);
+    assert_eq!(env_validation.max_level(), EnvValidationLevel::SubOptimal);
+    pretty_assertions::assert_eq!(
+        env_validation.issues(),
+        &[
+            EnvValidationIssue::SubOptimal(
+                EnvValidationIssueSubOptimal::CIInfoAuthorEmailTooShort(String::from(""),),
+            ),
+            EnvValidationIssue::SubOptimal(EnvValidationIssueSubOptimal::CIInfoAuthorNameTooShort(
+                String::from(""),
+            ),),
+            EnvValidationIssue::SubOptimal(
+                EnvValidationIssueSubOptimal::CIInfoCommitMessageTooShort(String::from(""),),
+            ),
+            EnvValidationIssue::SubOptimal(
+                EnvValidationIssueSubOptimal::CIInfoCommitterEmailTooShort(String::from(""),),
+            ),
+            EnvValidationIssue::SubOptimal(
+                EnvValidationIssueSubOptimal::CIInfoCommitterNameTooShort(String::from(""),),
+            ),
+            EnvValidationIssue::SubOptimal(EnvValidationIssueSubOptimal::CIInfoTitleTooShort(
+                String::from(""),
+            ),),
+        ]
+    );
+}
+
+#[test]
+fn test_simple_github_pr() {
+    let run_id = String::from("42069");
+    let pr_number = 123;
+    let actor = String::from("username");
+    let repository = String::from("test/tester");
+    let branch = String::from("some-branch-name");
+    let workflow = String::from("Pull Request");
+    let job = String::from("test-job");
+
+    let env_vars = EnvVars::from_iter(
+        vec![
+            (String::from("GITHUB_ACTIONS"), String::from("true")),
+            (
+                String::from("GITHUB_EVENT_NAME"),
+                String::from("pull_request"),
+            ),
+            (String::from("GITHUB_RUN_ID"), String::from(&run_id)),
+            (String::from("GITHUB_ACTOR"), String::from(&actor)),
+            (String::from("GITHUB_REPOSITORY"), String::from(&repository)),
+            (String::from("GITHUB_HEAD_REF"), String::from(&branch)),
+            (
+                String::from("GITHUB_REF"),
+                format!("refs/pull/{pr_number}/merge"),
+            ),
+            (String::from("GITHUB_WORKFLOW"), String::from(&workflow)),
+            (String::from("GITHUB_JOB"), String::from(&job)),
+        ]
+        .into_iter(),
+    );
+
+    let mut env_parser = EnvParser::new();
+    env_parser.parse(&env_vars).unwrap();
+
+    let ci_info = env_parser.into_ci_info_parser().unwrap().info_ci_info();
+
+    pretty_assertions::assert_eq!(
+        ci_info,
+        CIInfo {
+            platform: CIPlatform::GitHubActions,
+            job_url: Some(format!(
+                "https://github.com/{repository}/actions/runs/{run_id}?pr={pr_number}"
+            )),
+            branch: Some(branch),
+            branch_class: Some(BranchClass::PullRequest),
+            pr_number: Some(pr_number),
             actor: Some(actor),
             committer_name: None,
             committer_email: None,
