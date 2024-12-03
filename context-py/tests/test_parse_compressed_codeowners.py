75e58ef4d8f333d75493e4110b93b3bac26f9547
Tenest Hui Tang <hui@trunk.io>
2024-12-03 19:41:29 +0000

    remove unused imports

--- context-py/tests/test_parse_compressed_codeowners.py
+++ context-py/tests/test_parse_compressed_codeowners.py
@@ -1,12 +1,4 @@
 def test_parse_meta_from_tarball():
-    import io
-    import json
-    import tarfile
-    import tempfile
-
-    import zstandard as zstd
-    from botocore.response import StreamingBody
-
     from context_py import codeowners_parse
 
     codeowners_text = """* @trunk/test"""
