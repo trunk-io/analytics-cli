a021a6f6a5a842941d6a9dbb4217d41690d14382
gnalh <gabe@trunk.io>
2024-12-06 22:15:46 +0000

    init

---
+++ context/src/test_report/report.rs
@@ -0,0 +1,144 @@
+use chrono::prelude::*;
+#[cfg(feature = "ruby")]
+use magnus::{Module, Object};
+use prost_wkt_types::Timestamp;
+use proto::test_context::test_run::{TestCaseRun, TestCaseRunStatus, TestResult, UploaderMetadata};
+use std::cell::RefCell;
+#[cfg(feature = "wasm")]
+use wasm_bindgen::prelude::wasm_bindgen;
+
+const VERSION: &str = "0.0.1";
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct TestReport {
+    test_result: TestResult,
+}
+
+#[cfg_attr(feature = "wasm", wasm_bindgen(getter_with_clone))]
+#[cfg_attr(feature = "ruby", magnus::wrap(class = "TestReport"))]
+#[derive(Debug, Clone, PartialEq)]
+pub struct MutTestReport(RefCell<TestReport>);
+
+#[cfg_attr(feature = "wasm", wasm_bindgen)]
+impl MutTestReport {
+    pub fn new(origin: String) -> Self {
+        let mut test_result = TestResult::default();
+        test_result.uploader_metadata = Some(UploaderMetadata {
+            origin: origin,
+            version: VERSION.to_string(),
+        });
+        Self(RefCell::new(TestReport {
+            test_result: TestResult::default(),
+        }))
+    }
+
+    fn serialize_test_result(&self) -> Vec<u8> {
+        let test_result = self.0.borrow().test_result.clone();
+        let buf: Vec<u8> = prost::Message::encode_to_vec(&test_result);
+        buf
+    }
+
+    // sends out to the trunk api
+    pub fn publish(&self) -> Vec<u8> {
+        self.serialize_test_result()
+    }
+
+    // saves to local fs and prints the path
+    pub fn save(&self) -> String {
+        let buf = self.serialize_test_result();
+        // TODO - make this random
+        let path = "/tmp/test_report.bin";
+        std::fs::write(path, buf).unwrap_or_default();
+        path.to_string()
+    }
+
+    // adds a test to the test report
+    pub fn add_test(
+        &self,
+        name: String,
+        classname: String,
+        file: String,
+        parent_name: String,
+        line: i32,
+        status: String,
+        attempt: i32,
+        started_at: i64,
+        finished_at: i64,
+        output: String,
+    ) {
+        let mut test = TestCaseRun::default();
+        test.name = name;
+        test.classname = classname;
+        test.file = file;
+        test.parent_name = parent_name;
+        test.line = line;
+        match status.as_str() {
+            "success" => test.status = TestCaseRunStatus::Success.into(),
+            "failure" => test.status = TestCaseRunStatus::Failure.into(),
+            "skipped" => test.status = TestCaseRunStatus::Skipped.into(),
+            _ => test.status = TestCaseRunStatus::Unspecified.into(),
+        }
+        // test.status = status;
+        test.attempt = attempt;
+        let started_at_date_time = DateTime::from_timestamp(started_at, 0).unwrap_or_default();
+        let test_started_at = Timestamp {
+            seconds: started_at_date_time.timestamp(),
+            nanos: started_at_date_time.timestamp_subsec_nanos() as i32,
+        };
+        test.started_at = Some(test_started_at);
+        let finished_at_date_time = DateTime::from_timestamp(finished_at, 0).unwrap_or_default();
+        let test_finished_at = Timestamp {
+            seconds: finished_at_date_time.timestamp(),
+            nanos: finished_at_date_time.timestamp_subsec_nanos() as i32,
+        };
+        test.finished_at = Some(test_finished_at);
+        test.output_message = output;
+        self.0
+            .borrow_mut()
+            .test_result
+            .test_case_runs
+            .push(test.clone());
+    }
+
+    // lists the quarantined tests in the test report
+    pub fn list_quarantined_tests(&self) {
+        println!("List quarantined");
+    }
+
+    // validates the env is set for CI
+    pub fn valid_env(&self) {
+        println!("Valid env");
+    }
+
+    // validates that we are in a git repo
+    pub fn valid_git(&self) {
+        println!("Valid git");
+    }
+
+    pub fn to_string(&self) -> String {
+        self.clone().into()
+    }
+}
+
+impl Into<String> for MutTestReport {
+    fn into(self) -> String {
+        serde_json::to_string(&self.0.borrow().test_result).unwrap_or_default()
+    }
+}
+
+#[cfg(feature = "ruby")]
+pub fn ruby_init(ruby: &magnus::Ruby) -> Result<(), magnus::Error> {
+    let test_report = ruby.define_class("TestReport", ruby.class_object())?;
+    test_report.define_singleton_method("new", magnus::function!(MutTestReport::new, 1))?;
+    test_report.define_method("to_s", magnus::method!(MutTestReport::to_string, 0))?;
+    test_report.define_method("publish", magnus::method!(MutTestReport::publish, 0))?;
+    test_report.define_method("save", magnus::method!(MutTestReport::save, 0))?;
+    test_report.define_method("add_test", magnus::method!(MutTestReport::add_test, 10))?;
+    test_report.define_method(
+        "list_quarantined_tests",
+        magnus::method!(MutTestReport::list_quarantined_tests, 0),
+    )?;
+    test_report.define_method("valid_env", magnus::method!(MutTestReport::valid_env, 0))?;
+    test_report.define_method("valid_git", magnus::method!(MutTestReport::valid_git, 0))?;
+    Ok(())
+}
