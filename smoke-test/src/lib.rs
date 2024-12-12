ccb523fd5c298d47f6ebc156d5a1731c0224b2e5
Christian Millar <christian@trunk.io>
2024-12-12 22:48:03 +0000

    Remove failing test

--- smoke-test/src/lib.rs
+++ smoke-test/src/lib.rs
@@ -6,11 +6,6 @@ pub fn add(left: usize, right: usize) -> usize {
 mod tests {
 
     #[test]
-    fn always_fails() {
-        assert_eq!(1, 2)
-    }
-
-    #[test]
     fn always_succeeds() {
         assert_eq!(1, 1)
     }
