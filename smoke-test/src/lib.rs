6b66042a1cbca355802fb7c0ce0754d00386596d
Christian Millar <christian@trunk.io>
2024-12-17 22:54:14 +0000

    Changed the always failing test to only fail if a FAIL_TEST env var is set, so that we don't cause confusion for ci and local devs

--- smoke-test/src/lib.rs
+++ smoke-test/src/lib.rs
@@ -11,7 +11,12 @@ mod tests {
     }
 
     #[test]
-    fn always_fails() {
-        assert_eq!(1, 2)
+    fn fails_if_fail_env_is_present() {
+        // Using an env var so that we only fail when running smoke tests
+        if std::env::var("FAIL_TEST").is_ok() {
+            assert_eq!(1, 2)
+        } else {
+            assert_eq!(1, 1)
+        }
     }
 }
