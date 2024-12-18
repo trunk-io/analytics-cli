0f0b5d7b608ec03caad873c656f7eb373726ba1d
cmillar-trunk <christian@trunk.io>
2024-12-18 14:06:28 -0800

    [TRUNK-13813] Expand smoke test scope (#247)
    Adds quarantined test and production to smoke testing.

--- smoke-test/src/lib.rs
+++ smoke-test/src/lib.rs
@@ -9,4 +9,14 @@ mod tests {
     fn always_succeeds() {
         assert_eq!(1, 1)
     }
+
+    #[test]
+    fn fails_if_fail_env_is_present() {
+        // Using an env var so that we only fail when running smoke tests
+        if std::env::var("FAIL_TEST").is_ok() {
+            assert_eq!(1, 2)
+        } else {
+            assert_eq!(1, 1)
+        }
+    }
 }
