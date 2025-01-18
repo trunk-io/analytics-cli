b0e32d878635e221a9494856c8b7d1787bc8aea4
gnalh <gabe@trunk.io>
2025-01-18 00:36:53 +0000

    init

--- cli-tests/src/upload.rs
+++ cli-tests/src/upload.rs
@@ -1,23 +1,29 @@
+use std::sync::{Arc, Mutex};
 use std::{fs, io::BufReader};
 
 use crate::utils::{
     generate_mock_bazel_bep, generate_mock_codeowners, generate_mock_git_repo,
     generate_mock_valid_junit_xmls, CARGO_RUN,
 };
-use api::message::{BundleUploadStatus, CreateRepoRequest, UpdateBundleUploadRequest};
+use api::message::{
+    BundleUploadStatus, CreateBundleUploadRequest, CreateBundleUploadResponse, CreateRepoRequest,
+    GetQuarantineConfigRequest, GetQuarantineConfigResponse, UpdateBundleUploadRequest,
+};
 use assert_cmd::Command;
 use assert_matches::assert_matches;
+use axum::{extract::State, Json};
 use bundle::{BundleMeta, FileSetType};
 use codeowners::CodeOwners;
 use constants::{TRUNK_API_CLIENT_RETRY_COUNT_ENV, TRUNK_PUBLIC_API_ADDRESS_ENV};
 use context::{
     bazel_bep::parser::BazelBepParser, junit::parser::JunitParser, repo::RepoUrlParts as Repo,
 };
+use lazy_static::lazy_static;
 use predicates::prelude::*;
 use tempfile::tempdir;
 use test_utils::{
     inputs::get_test_file_path,
-    mock_server::{MockServerBuilder, RequestPayload},
+    mock_server::{MockServerBuilder, RequestPayload, SharedMockServerState},
 };
 
 // NOTE: must be multi threaded to start a mock server
@@ -578,4 +584,129 @@ async fn upload_bundle_with_no_junit_files_no_quarantine_successful_upload() {
 
     // HINT: View CLI output with `cargo test -- --nocapture`
     println!("{assert}");
+}
+
+#[tokio::test(flavor = "multi_thread")]
+async fn quarantines_tests_regardless_of_upload() {
+    let temp_dir = tempdir().unwrap();
+    generate_mock_git_repo(&temp_dir);
+    generate_mock_valid_junit_xmls(&temp_dir);
+    generate_mock_codeowners(&temp_dir);
+
+    let mut mock_server_builder = MockServerBuilder::new();
+
+    lazy_static! {
+        static ref QUARANTINE_CONFIG_RESPONSE: Arc<Mutex<QuarantineConfigResponse>> =
+            Arc::new(Mutex::new(QuarantineConfigResponse::None));
+    }
+    #[derive(Debug, Clone, Copy)]
+    enum QuarantineConfigResponse {
+        Disabled,
+        None,
+        Some,
+        All,
+    }
+    mock_server_builder.set_get_quarantining_config_handler(
+        |Json(get_quarantine_bulk_test_status_request): Json<GetQuarantineConfigRequest>| {
+            let mut test_ids = get_quarantine_bulk_test_status_request
+                .test_identifiers
+                .into_iter()
+                .map(|t| t.id)
+                .collect::<Vec<_>>();
+            let quarantine_config_response = *QUARANTINE_CONFIG_RESPONSE.lock().unwrap();
+            let quarantined_tests = match quarantine_config_response {
+                QuarantineConfigResponse::Disabled => Vec::new(),
+                QuarantineConfigResponse::None => Vec::new(),
+                QuarantineConfigResponse::Some => test_ids.split_off(1),
+                QuarantineConfigResponse::All => test_ids,
+            };
+            let is_disabled = matches!(
+                quarantine_config_response,
+                QuarantineConfigResponse::Disabled
+            );
+            async move {
+                Json(GetQuarantineConfigResponse {
+                    is_disabled,
+                    quarantined_tests,
+                })
+            }
+        },
+    );
+    #[derive(Debug, Clone, Copy)]
+    enum CreateBundleResponse {
+        Error,
+        Success,
+    }
+    lazy_static! {
+        static ref CREATE_BUNDLE_RESPONSE: Arc<Mutex<CreateBundleResponse>> =
+            Arc::new(Mutex::new(CreateBundleResponse::Error));
+    }
+    mock_server_builder.set_create_bundle_handler(
+        |State(state): State<SharedMockServerState>, _: Json<CreateBundleUploadRequest>| {
+            let create_bundle_response = *CREATE_BUNDLE_RESPONSE.lock().unwrap();
+            let result = match create_bundle_response {
+                CreateBundleResponse::Error => Err(String::from("Server is down")),
+                CreateBundleResponse::Success => {
+                    let host = &state.host;
+                    Ok(Json(CreateBundleUploadResponse {
+                        id: String::from("test-bundle-upload-id"),
+                        id_v2: String::from("test-bundle-upload-id-v2"),
+                        url: format!("{host}/s3upload"),
+                        key: String::from("unused"),
+                    }))
+                }
+            };
+            async { result }
+        },
+    );
+    let state = mock_server_builder.spawn_mock_server().await;
+
+    let args = &[
+        "upload",
+        "--junit-paths",
+        "./*",
+        "--org-url-slug",
+        "test-org",
+        "--token",
+        "test-token",
+    ];
+
+    let mut command = Command::new(CARGO_RUN.path());
+    command
+        .current_dir(&temp_dir)
+        .env(TRUNK_PUBLIC_API_ADDRESS_ENV, &state.host)
+        .env(TRUNK_API_CLIENT_RETRY_COUNT_ENV, "0")
+        .env("CI", "1")
+        .env("GITHUB_JOB", "test-job")
+        .args(args);
+
+    // First run won't quarantine any tests
+    *QUARANTINE_CONFIG_RESPONSE.lock().unwrap() = QuarantineConfigResponse::None;
+    *CREATE_BUNDLE_RESPONSE.lock().unwrap() = CreateBundleResponse::Error;
+    command.assert().failure();
+
+    // Second run quarantines all, but 1 test
+    *QUARANTINE_CONFIG_RESPONSE.lock().unwrap() = QuarantineConfigResponse::Some;
+    *CREATE_BUNDLE_RESPONSE.lock().unwrap() = CreateBundleResponse::Error;
+    command.assert().failure();
+
+    // Third run will not quarantine all tests because of upload failure
+    *QUARANTINE_CONFIG_RESPONSE.lock().unwrap() = QuarantineConfigResponse::All;
+    *CREATE_BUNDLE_RESPONSE.lock().unwrap() = CreateBundleResponse::Error;
+    command.assert().failure();
+
+    // Fourth run will quarantine all tests, and upload them
+    *QUARANTINE_CONFIG_RESPONSE.lock().unwrap() = QuarantineConfigResponse::All;
+    *CREATE_BUNDLE_RESPONSE.lock().unwrap() = CreateBundleResponse::Success;
+    command.assert().success();
+
+    // Fifth run will run with quarantining disabled, but will fail to upload
+    *QUARANTINE_CONFIG_RESPONSE.lock().unwrap() = QuarantineConfigResponse::Disabled;
+    *CREATE_BUNDLE_RESPONSE.lock().unwrap() = CreateBundleResponse::Error;
+    command.assert().failure();
+
+    // Sixth run will run with quarantining disabled, and will succeed with upload
+    *QUARANTINE_CONFIG_RESPONSE.lock().unwrap() = QuarantineConfigResponse::Disabled;
+    *CREATE_BUNDLE_RESPONSE.lock().unwrap() = CreateBundleResponse::Success;
+    command.assert().success();
 }
