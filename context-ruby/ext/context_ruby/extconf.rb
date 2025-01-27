514a0b91eefe51935a01ec3e8365254a4d387de4
gnalh <gabe@trunk.io>
2025-01-27 16:33:40 +0000

    add-gem-generation-context-ruby

--- context-ruby/ext/context_ruby/extconf.rb
+++ context-ruby/ext/context_ruby/extconf.rb
@@ -3,4 +3,14 @@
 require 'mkmf'
 require 'rb_sys/mkmf'
 
-create_rust_makefile('context_ruby/context_ruby')
+create_rust_makefile('context_ruby/context_ruby') do |r|
+  # For darwin multiple ranlibs are being packaged and the container chooses the wrong one.
+  # This is a workaround to force the correct ranlib to be used.
+  # https://github.com/cross-rs/cross/issues/1243#issuecomment-2102742482
+  case RUBY_PLATFORM
+  when 'arm64-darwin'
+    r.env = { 'RANLIB' => '/opt/osxcross/target/bin/arm64e-apple-darwin-ranlib' }
+  when 'x86_64-darwin'
+    r.env = { 'RANLIB' => '/opt/osxcross/target/bin/x86_64-apple-darwin-ranlib' }
+  end
+end
