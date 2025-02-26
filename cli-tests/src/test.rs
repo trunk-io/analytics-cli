470767a23ac023c4f7f0abb512d32b22be6c8f68
Christian Millar <christian@trunk.io>
2025-02-26 20:13:06 +0000

    Added tests for quarantining state

--- cli-tests/src/test.rs
+++ cli-tests/src/test.rs
@@ -1,11 +1,16 @@
 use std::{fs, io::BufReader};
 
+use api::message::{
+    CreateBundleUploadRequest, CreateBundleUploadResponse, GetQuarantineConfigRequest,
+    GetQuarantineConfigResponse,
+};
 use assert_matches::assert_matches;
+use axum::{extract::State, Json};
 use bundle::BundleMeta;
 use context::{bazel_bep::parser::BazelBepParser, junit::parser::JunitParser};
 use predicates::prelude::*;
 use tempfile::tempdir;
-use test_utils::mock_server::{MockServerBuilder, RequestPayload};
+use test_utils::mock_server::{MockServerBuilder, RequestPayload, SharedMockServerState};
 
 use crate::{
     command_builder::CommandBuilder,
@@ -205,4 +210,280 @@ async fn test_command_succeeds_with_bundle_using_bep() {
 
     // HINT: View CLI output with `cargo test -- --nocapture`
     println!("{assert}");
+}
+
+#[tokio::test(flavor = "multi_thread")]
+async fn quarantining_resets_fail_code() {
+    let temp_dir = tempdir().unwrap();
+    generate_mock_git_repo(&temp_dir);
+    generate_mock_valid_junit_xmls(&temp_dir);
+    generate_mock_codeowners(&temp_dir);
+
+    let mut mock_server_builder = MockServerBuilder::new();
+
+    mock_server_builder.set_get_quarantining_config_handler(
+        |Json(get_quarantine_bulk_test_status_request): Json<GetQuarantineConfigRequest>| {
+            let test_ids = get_quarantine_bulk_test_status_request
+                .test_identifiers
+                .into_iter()
+                .map(|t| t.id)
+                .collect::<Vec<_>>();
+            async move {
+                Json(GetQuarantineConfigResponse {
+                    is_disabled: false,
+                    quarantined_tests: test_ids,
+                })
+            }
+        },
+    );
+
+    mock_server_builder.set_create_bundle_handler(
+        |State(state): State<SharedMockServerState>, _: Json<CreateBundleUploadRequest>| {
+            async move {
+                let host = &state.host.clone();
+                Ok::<axum::Json<CreateBundleUploadResponse>, String>(Json(CreateBundleUploadResponse {
+                    id: String::from("test-bundle-upload-id"),
+                    id_v2: String::from("test-bundle-upload-id-v2"),
+                    url: format!("{host}/s3upload"),
+                    key: String::from("unused"),
+                }))
+            }
+        },
+    );
+    let state = mock_server_builder.spawn_mock_server().await;
+
+    CommandBuilder::test(
+        temp_dir.path(),
+        state.host.clone(),
+        vec![
+            String::from("bash"),
+            String::from("-c"),
+            String::from("touch ./*; exit 1"),
+        ],
+    )
+    .command()
+    .assert()
+    .success();
+}
+
+#[tokio::test(flavor = "multi_thread")]
+async fn quarantining_not_active_when_disable_quarantining_set() {
+    let temp_dir = tempdir().unwrap();
+    generate_mock_git_repo(&temp_dir);
+    generate_mock_valid_junit_xmls(&temp_dir);
+    generate_mock_codeowners(&temp_dir);
+
+    let mut mock_server_builder = MockServerBuilder::new();
+
+    mock_server_builder.set_get_quarantining_config_handler(
+        |Json(get_quarantine_bulk_test_status_request): Json<GetQuarantineConfigRequest>| {
+            let test_ids = get_quarantine_bulk_test_status_request
+                .test_identifiers
+                .into_iter()
+                .map(|t| t.id)
+                .collect::<Vec<_>>();
+            async move {
+                Json(GetQuarantineConfigResponse {
+                    is_disabled: false,
+                    quarantined_tests: test_ids,
+                })
+            }
+        },
+    );
+
+    mock_server_builder.set_create_bundle_handler(
+        |State(state): State<SharedMockServerState>, _: Json<CreateBundleUploadRequest>| {
+            async move {
+                let host = &state.host.clone();
+                Ok::<axum::Json<CreateBundleUploadResponse>, String>(Json(CreateBundleUploadResponse {
+                    id: String::from("test-bundle-upload-id"),
+                    id_v2: String::from("test-bundle-upload-id-v2"),
+                    url: format!("{host}/s3upload"),
+                    key: String::from("unused"),
+                }))
+            }
+        },
+    );
+    let state = mock_server_builder.spawn_mock_server().await;
+
+    CommandBuilder::test(
+        temp_dir.path(),
+        state.host.clone(),
+        vec![
+            String::from("bash"),
+            String::from("-c"),
+            String::from("touch ./*; exit 1"),
+        ],
+    )
+    .disable_quarantining(true)
+    .command()
+    .assert()
+    .failure();
+}
+
+#[tokio::test(flavor = "multi_thread")]
+async fn quarantining_not_active_when_use_quarantining_false() {
+    let temp_dir = tempdir().unwrap();
+    generate_mock_git_repo(&temp_dir);
+    generate_mock_valid_junit_xmls(&temp_dir);
+    generate_mock_codeowners(&temp_dir);
+
+    let mut mock_server_builder = MockServerBuilder::new();
+
+    mock_server_builder.set_get_quarantining_config_handler(
+        |Json(get_quarantine_bulk_test_status_request): Json<GetQuarantineConfigRequest>| {
+            let test_ids = get_quarantine_bulk_test_status_request
+                .test_identifiers
+                .into_iter()
+                .map(|t| t.id)
+                .collect::<Vec<_>>();
+            async move {
+                Json(GetQuarantineConfigResponse {
+                    is_disabled: false,
+                    quarantined_tests: test_ids,
+                })
+            }
+        },
+    );
+
+    mock_server_builder.set_create_bundle_handler(
+        |State(state): State<SharedMockServerState>, _: Json<CreateBundleUploadRequest>| {
+            async move {
+                let host = &state.host.clone();
+                Ok::<axum::Json<CreateBundleUploadResponse>, String>(Json(CreateBundleUploadResponse {
+                    id: String::from("test-bundle-upload-id"),
+                    id_v2: String::from("test-bundle-upload-id-v2"),
+                    url: format!("{host}/s3upload"),
+                    key: String::from("unused"),
+                }))
+            }
+        },
+    );
+    let state = mock_server_builder.spawn_mock_server().await;
+
+    CommandBuilder::test(
+        temp_dir.path(),
+        state.host.clone(),
+        vec![
+            String::from("bash"),
+            String::from("-c"),
+            String::from("touch ./*; exit 1"),
+        ],
+    )
+    .use_quarantining(false)
+    .command()
+    .assert()
+    .failure();
+}
+
+#[tokio::test(flavor = "multi_thread")]
+async fn quarantining_not_active_when_disable_true_but_use_true() {
+    let temp_dir = tempdir().unwrap();
+    generate_mock_git_repo(&temp_dir);
+    generate_mock_valid_junit_xmls(&temp_dir);
+    generate_mock_codeowners(&temp_dir);
+
+    let mut mock_server_builder = MockServerBuilder::new();
+
+    mock_server_builder.set_get_quarantining_config_handler(
+        |Json(get_quarantine_bulk_test_status_request): Json<GetQuarantineConfigRequest>| {
+            let test_ids = get_quarantine_bulk_test_status_request
+                .test_identifiers
+                .into_iter()
+                .map(|t| t.id)
+                .collect::<Vec<_>>();
+            async move {
+                Json(GetQuarantineConfigResponse {
+                    is_disabled: false,
+                    quarantined_tests: test_ids,
+                })
+            }
+        },
+    );
+
+    mock_server_builder.set_create_bundle_handler(
+        |State(state): State<SharedMockServerState>, _: Json<CreateBundleUploadRequest>| {
+            async move {
+                let host = &state.host.clone();
+                Ok::<axum::Json<CreateBundleUploadResponse>, String>(Json(CreateBundleUploadResponse {
+                    id: String::from("test-bundle-upload-id"),
+                    id_v2: String::from("test-bundle-upload-id-v2"),
+                    url: format!("{host}/s3upload"),
+                    key: String::from("unused"),
+                }))
+            }
+        },
+    );
+    let state = mock_server_builder.spawn_mock_server().await;
+
+    CommandBuilder::test(
+        temp_dir.path(),
+        state.host.clone(),
+        vec![
+            String::from("bash"),
+            String::from("-c"),
+            String::from("touch ./*; exit 1"),
+        ],
+    )
+    .disable_quarantining(true)
+    .use_quarantining(true)
+    .command()
+    .assert()
+    .failure();
+}
+
+#[tokio::test(flavor = "multi_thread")]
+async fn quarantining_not_active_when_disable_false_but_use_false() {
+    let temp_dir = tempdir().unwrap();
+    generate_mock_git_repo(&temp_dir);
+    generate_mock_valid_junit_xmls(&temp_dir);
+    generate_mock_codeowners(&temp_dir);
+
+    let mut mock_server_builder = MockServerBuilder::new();
+
+    mock_server_builder.set_get_quarantining_config_handler(
+        |Json(get_quarantine_bulk_test_status_request): Json<GetQuarantineConfigRequest>| {
+            let test_ids = get_quarantine_bulk_test_status_request
+                .test_identifiers
+                .into_iter()
+                .map(|t| t.id)
+                .collect::<Vec<_>>();
+            async move {
+                Json(GetQuarantineConfigResponse {
+                    is_disabled: false,
+                    quarantined_tests: test_ids,
+                })
+            }
+        },
+    );
+
+    mock_server_builder.set_create_bundle_handler(
+        |State(state): State<SharedMockServerState>, _: Json<CreateBundleUploadRequest>| {
+            async move {
+                let host = &state.host.clone();
+                Ok::<axum::Json<CreateBundleUploadResponse>, String>(Json(CreateBundleUploadResponse {
+                    id: String::from("test-bundle-upload-id"),
+                    id_v2: String::from("test-bundle-upload-id-v2"),
+                    url: format!("{host}/s3upload"),
+                    key: String::from("unused"),
+                }))
+            }
+        },
+    );
+    let state = mock_server_builder.spawn_mock_server().await;
+
+    CommandBuilder::test(
+        temp_dir.path(),
+        state.host.clone(),
+        vec![
+            String::from("bash"),
+            String::from("-c"),
+            String::from("touch ./*; exit 1"),
+        ],
+    )
+    .disable_quarantining(false)
+    .use_quarantining(false)
+    .command()
+    .assert()
+    .failure();
 }
