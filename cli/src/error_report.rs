cf1bcc61d19bcf68e33c86f8181d5f9f954d892d
gnalh <gabe@trunk.io>
2025-03-04 07:05:46 +0000

    init

--- cli/src/error_report.rs
+++ cli/src/error_report.rs
@@ -33,6 +33,22 @@ pub fn log_error(error: &anyhow::Error, base_message: Option<&str>) -> i32 {
     exitcode::SOFTWARE
 }
 
+pub fn error_reason(error: &anyhow::Error) -> String {
+    let root_cause = error.root_cause();
+    if let Some(io_error) = root_cause.downcast_ref::<std::io::Error>() {
+        if io_error.kind() == std::io::ErrorKind::ConnectionRefused {
+            return "connection".to_string();
+        }
+    }
+
+    if let Some(reqwest_error) = root_cause.downcast_ref::<reqwest::Error>() {
+        if let Some(status) = reqwest_error.status() {
+            return status.to_string().replace(' ', "_").to_lowercase();
+        }
+    }
+    "unknown".into()
+}
+
 fn message(base_message: Option<&str>, hint: &str) -> String {
     match base_message {
         Some(base_message) => format!("{} because {}", base_message, hint),
