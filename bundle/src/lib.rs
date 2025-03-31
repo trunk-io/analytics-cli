ba3fe393fc39798d2826eadc5958ef8acca669ad
Gabe <78751069+gnalh@users.noreply.github.com>
2025-03-31 11:13:34 -0700

    Revert "Remove tags from being set (#504)"
    This reverts commit 149cd9930b25a2e4303d40872e8fa91f4d276662.

--- bundle/src/lib.rs
+++ bundle/src/lib.rs
@@ -1,9 +1,11 @@
 mod bundle_meta;
 mod bundler;
+mod custom_tag;
 mod files;
 mod types;
 
 pub use bundle_meta::*;
 pub use bundler::*;
+pub use custom_tag::*;
 pub use files::*;
 pub use types::*;
