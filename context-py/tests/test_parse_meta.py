7c206226adb7c3fd8fb4f3d096a6d912288f6710
Dylan Frankland <dfrankland@users.noreply.github.com>
2025-06-11 11:03:33 -0700

    add json dumping to `BindingsVersionedBundle`

--- context-py/tests/test_parse_meta.py
+++ context-py/tests/test_parse_meta.py
@@ -179,3 +179,97 @@ def test_parse_meta_invalid():
         _ = parse_meta(encoded_meta)
 
     assert "missing field `version` at" in str(excinfo.value)
+
+
+def test_parse_and_dump_meta_roundtrip():
+    import json
+    import typing as PT
+
+    from context_py import parse_meta
+
+    valid_meta: dict[str, PT.Any] = {
+        "bundle_upload_id": "59c8ddd9-0a00-4b56-9eea-ef0d60ebcb79",
+        "cli_version": "cargo=0.5.11 git=7e5824fa365c63a2d4b38020762be17f4edd6425 rustc=1.80.0-nightly",
+        "codeowners": None,
+        "envs": {},
+        "file_sets": [
+            {
+                "file_set_type": "Junit",
+                "files": [
+                    {
+                        # serde_json doesn't support u128 in #[serde(flatten)] yet
+                        "last_modified_epoch_ns": 0,
+                        "original_path": "/home/runner/work/trunk/test/file1.xml",
+                        "original_path_rel": None,
+                        "owners": [],
+                        "path": "junit/0",
+                        "team": "",
+                    },
+                    {
+                        # serde_json doesn't support u128 in #[serde(flatten)] yet
+                        "last_modified_epoch_ns": 0,
+                        "original_path": "/home/runner/work/trunk/test/file2.xml",
+                        "original_path_rel": None,
+                        "owners": [],
+                        "path": "junit/1",
+                        "team": "",
+                    },
+                ],
+                "glob": "junit.xml",
+            },
+            {
+                "file_set_type": "Junit",
+                "files": [
+                    {
+                        # serde_json doesn't support u128 in #[serde(flatten)] yet
+                        "last_modified_epoch_ns": 0,
+                        "original_path": "/home/runner/work/trunk/test/file1.xml",
+                        "original_path_rel": None,
+                        "owners": [],
+                        "path": "junit/0",
+                        "team": "",
+                    },
+                    {
+                        # serde_json doesn't support u128 in #[serde(flatten)] yet
+                        "last_modified_epoch_ns": 0,
+                        "original_path": "/home/runner/work/trunk/test/file2.xml",
+                        "original_path_rel": None,
+                        "owners": [],
+                        "path": "junit/1",
+                        "team": "",
+                    },
+                ],
+                "glob": "junit.xml",
+                "resolved_end_time_epoch_ms": 1749505703092,
+                "resolved_start_time_epoch_ms": 1749505703092,
+                "resolved_status": "Passed",
+            },
+        ],
+        "org": "trunk",
+        "os_info": "linux",
+        "quarantined_tests": [],
+        "repo": {
+            "repo": {"host": "github.com", "name": "test", "owner": "trunk"},
+            "repo_head_author_email": "42547082+deepsource-io[bot]@users.noreply.github.com",
+            "repo_head_author_name": "deepsource-io[bot]",
+            "repo_head_branch": "refs/heads/main",
+            "repo_head_commit_epoch": 1720652103,
+            "repo_head_commit_message": "ci: add .deepsource.toml",
+            "repo_head_sha": "74518d470d8cfeb41408a85cf6097bb7f09ad902",
+            "repo_head_sha_short": None,
+            "repo_root": "/home/runner/work/trunk/test",
+            "repo_url": "https://github.com/trunk/test",
+        },
+        "schema": "V0_5_29",
+        "tags": [],
+        "test_command": None,
+        "upload_time_epoch": 1721095230,
+        "version": "1",
+    }
+
+    valid_meta_str = json.dumps(valid_meta, sort_keys=True)
+    bundle_meta = parse_meta(valid_meta_str.encode())
+    assert (
+        json.dumps(json.loads(bundle_meta.dump_json()), sort_keys=True)
+        == valid_meta_str
+    )
