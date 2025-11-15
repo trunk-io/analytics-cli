c164e5aa5d963faa8e8ce0b2bb0b01fb8b541245
gnalh <gabe@trunk.io>
2025-11-14 16:07:24 -0800

    idk

--- context-ruby/ext/context_ruby/extconf.rb
+++ context-ruby/ext/context_ruby/extconf.rb
@@ -9,10 +9,11 @@ create_rust_makefile('context_ruby/context_ruby') do |r|
   # https://github.com/cross-rs/cross/issues/1243#issuecomment-2102742482
   r.profile = ENV.fetch('RB_SYS_CARGO_PROFILE', :dev).to_sym
   puts "Using profile: #{r.profile}"
+  r.env = { 'CARGO_PROFILE_RELEASE_OPT_LEVEL' => '1' }
   case RUBY_PLATFORM
   when 'arm64-darwin'
-    r.env = { 'RANLIB' => '/opt/osxcross/target/bin/arm64e-apple-darwin-ranlib' }
+    r.env['RANLIB'] = '/opt/osxcross/target/bin/arm64e-apple-darwin-ranlib'
   when 'x86_64-darwin'
-    r.env = { 'RANLIB' => '/opt/osxcross/target/bin/x86_64-apple-darwin-ranlib' }
+    r.env['RANLIB'] = '/opt/osxcross/target/bin/x86_64-apple-darwin-ranlib'
   end
 end
