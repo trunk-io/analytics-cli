b399b8122687672a5dc130a5629c8fe0112a2e33
gnalh <gabe@trunk.io>
2025-12-15 15:26:39 -0800

    better tests

--- context-js/tests/meta.test.ts
+++ context-js/tests/meta.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, it } from "vitest";
-import { gen_info_id } from "../pkg/context_js";
+import { gen_info_id, gen_info_id_base } from "../pkg/context_js";
 
 describe("context-js", () => {
   // These tests match the tests in context/src/meta/id.rs.
@@ -9,8 +9,8 @@ describe("context-js", () => {
     it("generates ID properly for trunk", () => {
       expect.hasAssertions();
 
-      const generateIdForTest = () =>
-        gen_info_id(
+      const generateIdForTest = () => ({
+        id: gen_info_id(
           "example_org",
           "example_repo",
           "src/lib.rs",
@@ -19,24 +19,36 @@ describe("context-js", () => {
           "example_name",
           "trunk:12345",
           "unix",
-        );
+        ),
+        base_id: gen_info_id_base(
+          "example_org",
+          "example_repo",
+          "src/lib.rs",
+          "ExampleClass",
+          "parent/fact/path",
+          "example_name",
+          "trunk:12345",
+          "unix",
+        ),
+      });
 
       const result = generateIdForTest();
 
-      expect(result).toBe("db8c5727-0fe9-560f-863f-7f3ee68df425");
+      expect(result.base_id).toBe("4392f63c-8dc9-5cec-bbdc-e7b90c2e5a6b");
+      expect(result.id).toBe("db8c5727-0fe9-560f-863f-7f3ee68df425");
 
       // Generate again to ensure it is consistent
       const result2 = generateIdForTest();
 
-      expect(result2).toBe(result);
+      expect(result2).toStrictEqual(result);
     });
 
     it("works properly with existing v5 UUID", () => {
       expect.hasAssertions();
 
       const existingInfoId = "a6e84936-3ee9-57d5-b041-ae124896f654";
-      const generateIdForTest = ({ variant = "" }: { variant?: string }) =>
-        gen_info_id(
+      const generateIdForTest = ({ variant = "" }: { variant?: string }) => ({
+        id: gen_info_id(
           "example_org",
           "example_repo",
           "src/lib.rs",
@@ -45,28 +57,43 @@ describe("context-js", () => {
           "example_name",
           existingInfoId,
           variant,
-        );
+        ),
+        base_id: gen_info_id_base(
+          "example_org",
+          "example_repo",
+          "src/lib.rs",
+          "ExampleClass",
+          "parent/fact/path",
+          "example_name",
+          existingInfoId,
+          variant,
+        ),
+      });
 
       const result = generateIdForTest({});
 
-      expect(result).toBe(existingInfoId);
+      expect(result.id).toBe(existingInfoId);
+      expect(result.base_id).toBe(existingInfoId);
 
       // Generate again to ensure it is consistent
       const result2 = generateIdForTest({});
 
-      expect(result2).toBe(result);
+      expect(result2).toStrictEqual(result);
 
       // Adding a variant changs the ID.
       const resultWithVariant = generateIdForTest({ variant: "unix" });
 
-      expect(resultWithVariant).toBe("931cae54-0fcd-56eb-8eac-afa833699e53");
+      expect(resultWithVariant.id).toBe("931cae54-0fcd-56eb-8eac-afa833699e53");
+      expect(resultWithVariant.base_id).toBe(
+        "8057218b-95e4-5373-afbe-c366d4058615",
+      );
     });
 
     it("works properly without existing v5 UUID", () => {
       expect.hasAssertions();
 
-      const generateIdForTest = ({ infoId }: { infoId?: string }) =>
-        gen_info_id(
+      const generateIdForTest = ({ infoId }: { infoId?: string }) => ({
+        id: gen_info_id(
           "example_org",
           "example_repo",
           "src/lib.rs",
@@ -75,23 +102,35 @@ describe("context-js", () => {
           "example_name",
           infoId,
           "unix",
-        );
+        ),
+        base_id: gen_info_id_base(
+          "example_org",
+          "example_repo",
+          "src/lib.rs",
+          "ExampleClass",
+          "parent/fact/path",
+          "example_name",
+          infoId,
+          "unix",
+        ),
+      });
 
       const result = generateIdForTest({});
 
-      expect(result).toBe("1bf61475-b542-5faf-aa85-e66a691257a3");
+      expect(result.base_id).toBe("c869cb93-66e2-516d-a0ea-15ff4b413c3f");
+      expect(result.id).toBe("1bf61475-b542-5faf-aa85-e66a691257a3");
 
       // Generate again to ensure it is consistent
       const result2 = generateIdForTest({});
 
-      expect(result2).toBe(result);
+      expect(result2).toStrictEqual(result);
 
       // Existing UUID is ignored if it isn't V5
       const resultForV4Uuid = generateIdForTest({
         infoId: "08e1c642-3a55-45cf-8bf9-b9d0b21785dd", // V4
       });
 
-      expect(resultForV4Uuid).toBe(result);
+      expect(resultForV4Uuid).toStrictEqual(result);
     });
 
     it("doesn't change non-variant case", () => {
