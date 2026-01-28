01ed5de7eded29e3d2fa23fbd0507a3bd0297beb
Tyler Jang <tyler@trunk.io>
2026-01-28 20:21:10 +0000

    almost there

--- rspec-trunk-flaky-tests/lib/trunk_spec_helper.rb
+++ rspec-trunk-flaky-tests/lib/trunk_spec_helper.rb
@@ -65,13 +65,12 @@ end
 
 def knapsack_detector_command?
   command_line = "#{$PROGRAM_NAME} #{ARGV.join(' ')}".strip
-  # donotland
-  # command_line.include?('knapsack_pro:rspec_test_example_detector')
-  if command_line.include?('knapsack_pro:rspec_test_example_detector')
-    # knapsack_pro:queue:rspec:initialize
+  if command_line.include?('knapsack_pro:rspec_test_example_detector') || command_line.include?('knapsack_pro:queue:rspec:initialize')
+    # donotland
     puts "Detected knapsack example detector command: #{command_line}\n\n".yellow
     true
   else
+    # donotland
     puts "Detected knapsack other command: #{command_line}\n\n".yellow
     false
   end
