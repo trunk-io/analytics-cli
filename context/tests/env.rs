e83aadcf921607a826d65c6b0f7ebe1881584804
Tyler Jang <tyler@trunk.io>
2026-04-28 18:41:03 +0000

    handle circle ci prs properly

--- context/tests/env.rs
+++ context/tests/env.rs
@@ -1441,6 +1441,55 @@ fn test_circleci_pr() {
 }
 
 #[test]
+fn test_circleci_pull_request_url_fallback() {
+    let branch = String::from("feature/add-feature");
+    let build_url = String::from("https://circleci.com/gh/my-org/my-repo/456");
+    let workflow_id = String::from("workflow-def-456");
+    let job_name = String::from("build-and-test");
+    let username = String::from("janedoe");
+    let pr_number = 26558;
+    let pull_request_url = format!("https://github.com/my-org/my-repo/pull/{pr_number}");
+
+    let env_vars = EnvVars::from_iter(vec![
+        (String::from("CIRCLECI"), String::from("true")),
+        (String::from("CIRCLE_BRANCH"), String::from(&branch)),
+        (String::from("CIRCLE_BUILD_URL"), String::from(&build_url)),
+        (
+            String::from("CIRCLE_WORKFLOW_ID"),
+            String::from(&workflow_id),
+        ),
+        (String::from("CIRCLE_JOB"), String::from(&job_name)),
+        (String::from("CIRCLE_USERNAME"), String::from(&username)),
+        (String::from("CIRCLE_PULL_REQUEST"), pull_request_url),
+    ]);
+
+    let mut env_parser = EnvParser::new();
+    env_parser.parse(&env_vars, &[], None);
+
+    let ci_info = env_parser.into_ci_info_parser().unwrap().info_ci_info();
+
+    pretty_assertions::assert_eq!(
+        ci_info,
+        CIInfo {
+            platform: CIPlatform::CircleCI,
+            job_url: Some(build_url),
+            branch: Some(branch),
+            branch_class: Some(BranchClass::PullRequest),
+            pr_number: Some(pr_number),
+            actor: Some(username),
+            committer_name: None,
+            committer_email: None,
+            author_name: None,
+            author_email: None,
+            commit_message: None,
+            title: None,
+            workflow: Some(workflow_id),
+            job: Some(job_name),
+        }
+    );
+}
+
+#[test]
 fn test_circleci_stable_branch() {
     let branch = String::from("main");
     let build_url = String::from("https://circleci.com/gh/my-org/my-repo/789");
